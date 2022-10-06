9db9fa51ac1bc6c95b0baa265a6b384757cd8e3f
Uktamjon Komilov <uktamjon_komillov@epam.com>
2022-10-06 16:06:33 +0500

    Trip list and detail view

--- server/taxi/settings.py
+++ server/taxi/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import datetime
 from dotenv import load_dotenv
 
 load_dotenv(".env")
@@ -110,4 +111,18 @@ CHANNEL_LAYERS = {
             "hosts": [REDIS_URL]
         }
     }
+}
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": datetime.timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": datetime.timedelta(days=1),
+    "USER_ID_CLAIM": "id",
 }
--- server/trips/views.py
+++ server/trips/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, viewsets, permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .serializers import LogInSerializer, UserSerializer, TripSerializer
+from .models import Trip
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
+
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = "id"
+    lookup_url_kwarg = "trip_id"
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
